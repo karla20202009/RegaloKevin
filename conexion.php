9eadcc58a9eee4bcb302fce7832d0f77d19afcfb
karla20202009 <karla.rdzg.2020@gmail.com>
2026-05-06 22:23:00 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,11 +1,12 @@
 <?php
-// Datos de tu panel de InfinityFree (MySQL Databases)
-$host = "sql213.infinityfree.com"; // Reemplaza el XXX por el tuyo
-$user = "if0_41851722";           // Tu usuario de MySQL
-$pass = "0GkHhvN1WKw3X";         // Tu contraseña de la cuenta
-$db   = "if0_41851722_kvn";    // El nombre de la base de datos que creaste
+// Railway nos da estos datos solitos
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
-$conexion = mysqli_connect($host, $user, $pass, $db);
+$conexion = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conexion) {
     die("Error de conexión: " . mysqli_connect_error());
